cb2b886620bd4f0a07e83c54fecd31c570f8b526
Emiloju Temitope Emmanuel <masterleeman@gmail.com>
2022-06-25 22:41:08 +0100

    added files

---
+++ me.js
@@ -0,0 +1,7 @@
+let name = "Emiloju Temitope Emmanuel";
+let height = "5.8'";
+let country = "Nigeria";
+alert("My name is "+ name);
+alert("My height is "+ height);
+alert("I am from "+ country);
+ 
